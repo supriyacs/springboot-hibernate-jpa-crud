be8bbf0d9d6db47199a7c4c0ab2e48f602791a10
Supriya <supriyasubramanian92@gmail.com>
2024-02-08 13:00:47 +0530

    first commit

---
+++ src/main/java/com/SCS/cruddemo/CruddemoApplication.java
@@ -0,0 +1,21 @@
+package com.SCS.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner CommandLineRunner(String[] args){
+		return runner->{
+			System.out.println("Hello world");
+		};
+	}
+}
