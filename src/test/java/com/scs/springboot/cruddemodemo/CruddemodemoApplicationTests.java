cab4382ef0e89f3ed1e35f903d6064180c9d6f1a
Supriya <supriyasubramanian92@gmail.com>
2024-03-14 07:21:31 +0530

    initial commit

---
+++ src/test/java/com/scs/springboot/cruddemodemo/CruddemodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.springboot.cruddemodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
